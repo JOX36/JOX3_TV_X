07ff1ce985fd39206b68612e41315f34d79fb86e
JOX36 <angelvalenciamurcielago@gmail.com>
2026-06-19 23:27:11 -0500

    Create JoxTvApp class extending Application

---
+++ app/src/main/java/com/jox3/tv/JoxTvApp.java
@@ -0,0 +1,10 @@
+package com.jox3.tv;
+
+import android.app.Application;
+
+public class JoxTvApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
