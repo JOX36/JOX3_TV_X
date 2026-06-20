8bdb92b8ea239c505c9bca03ac231e2f75b6cb49
JOX36 <angelvalenciamurcielago@gmail.com>
2026-06-20 07:26:07 -0500

    Add settings button functionality in HomeActivity

--- app/src/main/java/com/jox3/tv/ui/home/HomeActivity.java
+++ app/src/main/java/com/jox3/tv/ui/home/HomeActivity.java
@@ -1,13 +1,22 @@
 package com.jox3.tv.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import com.jox3.tv.R;
+import com.jox3.tv.ui.settings.SettingsActivity;
 
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View btnSettings = findViewById(R.id.btn_open_settings);
+        if (btnSettings != null) {
+            btnSettings.setOnClickListener(v ->
+                    startActivity(new Intent(this, SettingsActivity.class)));
+        }
     }
 }
